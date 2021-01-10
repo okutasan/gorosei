37e47978a897433aa30b988b0df1de7ba6b4bc30
okutasan <oktavanhowen@gmail.com>
2021-01-11 01:53:04 +0700

    variable 2

--- variable.go
+++ variable.go
@@ -10,4 +10,21 @@ func main()  {
 
 	name = "Okta Cahyawan"
 	fmt.Println(name)
+
+	var waifu = "Arisa"
+	fmt.Println(waifu)
+
+	var umur = 19
+	fmt.Println(umur)
+
+	negara := "Japan"
+	fmt.Println(negara)
+
+	var (
+		namaDepan = "Okta Arif"
+		namaBelakang = "Cahyawan"
+	)
+
+	fmt.Println(namaDepan)
+	fmt.Println(namaBelakang)
 }
