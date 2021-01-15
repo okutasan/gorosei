e54daa1079542f65a0c116aa744cd154425d25fc
okutasan <oktavanhowen@gmail.com>
2021-01-15 22:54:40 +0700

    konstanta

---
+++ constanta.go
@@ -0,0 +1,14 @@
+package main
+
+func main()  {
+	const firstname string = "Okta"
+	const lastname =  "Arif"
+
+	const (
+		negara = "Indonesia"
+		benua = "asia"
+	)
+	//
+	//firstname = "ubah"
+	//lastname = "ubah bisa tidak ?"
+}
